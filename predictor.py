525e4f2dbb0d60ffb95a5b07c63256ed05927fb0
manishasodekar <manish@healiom.com>
2024-09-04 18:58:42 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -25,7 +25,8 @@ model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.float16,
     # attn_implementation="flash_attention_2",  # Enable Flash Attention v2
     device_map="auto"
-).to(device)
+)
+# .to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
